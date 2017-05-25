5a8bead1b216f5ca75cd51c51dace99697ad96dd
kyuwankim <lpnids@gmail.com>
2017-05-25 12:19:15 +0900

    1. CustomAdapter 2. inner create Holder extends View.ViewHolder 3. constructor

--- app/src/main/java/com/kyuwankim/android/recycler/MainActivity.java
+++ app/src/main/java/com/kyuwankim/android/recycler/MainActivity.java
@@ -3,16 +3,59 @@ package com.kyuwankim.android.recycler;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+RecyclerView listview ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listview = (RecyclerView)findViewById(R.id.recyclerview);
+
+        // 1. 데이터
+        ArrayList<Data> datas = Loader.getData(this);
+
+        // 2. 아답터
+
+        // 3. 연결
+
+        // 4. 레이아웃 매니저
     }
+}
+
+class CustomRecycler extends RecyclerView.Adapter<CustomRecycler.Holder>{
+
+
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class Holder extends RecyclerView.ViewHolder{
+
+        public Holder(View itemView) {
+            super(itemView);
+        }
+    }
+
 }
 
 class Loader {
@@ -21,9 +64,9 @@ class Loader {
         for (int i = 1; i <= 10; i++) {
             Data data = new Data();
             data.no = i;
-            data.title = "자동차";
+            data.title = "안드로보이";
 
-            data.setImage("car" + i, context);
+            data.setImage("ic_launcher" + i, context);
 
             result.add(data);
         }
