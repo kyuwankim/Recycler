39bdc735998ee03cce33da69e940fe3a1fcd2034
kyuwankim <lpnids@gmail.com>
2017-05-25 12:00:37 +0900

    Add Data, Loader

--- app/src/main/java/com/kyuwankim/android/recycler/MainActivity.java
+++ app/src/main/java/com/kyuwankim/android/recycler/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kyuwankim.android.recycler;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+}
+
+class Loader {
+    public static ArrayList<Data> getData(Context context) {
+        ArrayList<Data> result = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            Data data = new Data();
+            data.no = i;
+            data.title = "자동차";
+
+            data.setImage("car" + i, context);
+
+            result.add(data);
+        }
+        return result;
+    }
+}
+
+class Data {
+    public int no;
+    public String title;
+    public String image;
+    public int resId;
+
+    public void setImage(String str, Context context) {
+        image = str;
+        // 문자열로 리소스 아이디 가져오기
+        resId = context.getResources().getIdentifier(image, "mipmap", context.getPackageName());
     }
 }
